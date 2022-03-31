3c6c91412c252976253a6340a4e7d1951d5969d2
lolotrgeek <jon@simplyenable.com>
2022-03-31 15:23:14 -0500

    feat: clear cache command, error handling

--- src/finder.js
+++ src/finder.js
@@ -1,7 +1,9 @@
 const Keyv = require('keyv')
 const { KeyvFile } = require('keyv-file')
 const { Requester } = require('./requester')
-const path = require('path')
+const { unlink } = require('fs').promises
+const { join } = require('path')
+const { rejects } = require('assert')
 
 /**
  * Looks for cached data based on type, otherwise executes a request query
@@ -11,26 +13,29 @@ const path = require('path')
  * @param {object} store - Storage adapter, defaults to KeyvFile
  * @returns 
  */
-function findCachedOrRequest(query, type, timeout, store= new KeyvFile({ filename: path.join(__dirname, '../../../', `/data/${type}.json`)})) {
-    if (typeof query !== 'string' || query === 'true') {
-        return null
-    }
-    if (typeof type !== 'string') {
-        return null
-    }
+function findCachedOrRequest(query, type, timeout, store = new KeyvFile({ filename: join(__dirname, '../../../', `/data/${type}.json`) })) {
+    if (typeof query !== 'string' || query === 'true') return null
+    if (typeof type !== 'string') return null
+
     const keyv = new Keyv({ store })
-    return new Promise(async resolve => {
+    return new Promise(async (resolve, reject) => {
         try {
+            if (query === 'clear') unlink(join(__dirname, '../../../', `/data/${type}.json`)).then(resolve).catch(reject)
             let data = await keyv.get(`${type}`)
             if (!data) throw "no data"
             console.log(`Found cached data for ${type}`, data)
             resolve(data)
         } catch (error) {
             Requester(query, async data => {
-                console.log(`Requesting data for ${type}`)
-                if(!timeout) await keyv.set(`${type}`, data)
-                else await keyv.set(`${type}`, data, timeout)
-                resolve(data)
+                try {
+                    console.log(`Requesting data for ${type}`)
+                    if (!timeout) await keyv.set(`${type}`, data)
+                    else await keyv.set(`${type}`, data, timeout)
+                    resolve(data)
+                } catch (error) {
+                    reject(error)
+                }
+
             })
         }
     })

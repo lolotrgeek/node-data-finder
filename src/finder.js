3840323c4b4d189e52043113414ec48d8fe9cb3c
lolotrgeek <jon@simplyenable.com>
2022-04-01 05:53:03 -0500

    refactor: functional

--- src/finder.js
+++ src/finder.js
@@ -3,7 +3,6 @@ const { KeyvFile } = require('keyv-file')
 const { Requester } = require('./requester')
 const { unlink } = require('fs').promises
 const { join } = require('path')
-const { rejects } = require('assert')
 
 /**
  * Looks for cached data based on type, otherwise executes a request query
@@ -13,32 +12,55 @@ const { rejects } = require('assert')
  * @param {object} store - Storage adapter, defaults to KeyvFile
  * @returns 
  */
-function findCachedOrRequest(query, type, timeout, store = new KeyvFile({ filename: join(__dirname, '../../../', `/data/${type}.json`) })) {
-    if (typeof query !== 'string' || query === 'true') return null
-    if (typeof type !== 'string') return null
-
-    const keyv = new Keyv({ store })
+function findCachedOrRequest(query, type, timeout, store) {
     return new Promise(async (resolve, reject) => {
         try {
-            if (query === 'clear') unlink(join(__dirname, '../../../', `/data/${type}.json`)).then(resolve).catch(reject)
+            if(validate(query, type) === false) reject("Invalid")
+            if (query === 'clear') {
+                await clear(type)
+                resolve("cleared")
+            }
+            const keyv = Store(type, store)
             let data = await keyv.get(`${type}`)
-            if (!data) throw "no data"
-            console.log(`Found cached data for ${type}`, data)
+            if(data) {
+                console.log(`Found cached data for ${type}`, data)
+                resolve(data)
+            }
+            else {
+                data = await Request(type, timeout)
+                resolve(data)
+            }
+        } catch (error) {reject(error)}
+    })
+}
+
+function Request(type, timeout){
+    return Requester(query, async data => {
+        try {
+            console.log(`Requesting data for ${type}`)
+            if (!timeout) await keyv.set(type, data)
+            else await keyv.set(type, data, timeout)
             resolve(data)
         } catch (error) {
-            Requester(query, async data => {
-                try {
-                    console.log(`Requesting data for ${type}`)
-                    if (!timeout) await keyv.set(`${type}`, data)
-                    else await keyv.set(`${type}`, data, timeout)
-                    resolve(data)
-                } catch (error) {
-                    reject(error)
-                }
-
-            })
+            reject(error)
         }
-    })
+    })    
+}
+
+function Store(type, store) {
+    if (!store) store = new KeyvFile({ filename: join(__dirname, '../../../', `/data/${type}.json`) })
+    const keyv = new Keyv({ store })
+    return keyv
+}
+
+function validate(query, type) {
+    if (typeof query !== 'string' || query === 'true') return false
+    if (typeof type !== 'string') return false
+    return true
+}
+
+function clear(type) {
+    return unlink(join(__dirname, '../../../', `/data/${type}.json`))
 }
 
 module.exports = { findCachedOrRequest }

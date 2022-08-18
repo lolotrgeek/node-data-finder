4011e50dc7e2983af1e60efff18b7bf61c49c4f4
lolotrgeek <jon@simplyenable.com>
2022-08-18 08:03:34 -0500

    test: update expires

--- tests/expires_test.js
+++ tests/expires_test.js
@@ -2,13 +2,19 @@ const { find_test } = require("./find_test")
 
 let timeout = 1000
 
-find_test('raw', 'clear').then(cleared => {
-    let start = Date.now()
-    find_test("raw", "https://url", timeout, 'expires').then(result => {
-        let end = Date.now()
-        let runtime = end - start
-        let expected = (start + timeout)
-        let actual_is_expected = result.expires - expected - runtime
-        console.log(actual_is_expected === 0)
+async function expires_test() {
+    return new Promise(resolve => {
+        find_test('raw', 'clear').then(cleared => {
+            let start = Date.now()
+            find_test("raw", "https://url", timeout, 'expires').then(result => {
+                let end = Date.now()
+                let runtime = end - start
+                let expected = (start + timeout)
+                let actual_is_expected = result.expires - expected - runtime
+                resolve(actual_is_expected <= 2 && actual_is_expected >= -2)
+            })
+        })
     })
-})
+}
+
+module.exports = { expires_test }
